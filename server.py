b6434c8812deac422b0dd23d0d66effc8fd61107
mischmit <michael.schmitt@igd.fraunhofer.de>
2015-03-10 22:45:52 +0100

    - Added Template for the html file - First simple implementation works :) (no transition, no precaching)

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import SimpleHTTPServer
 from BaseHTTPServer import HTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import datetime
+import string
 
 matches = []
 allowedExtensions = ('.jpg', '.jpeg', '.JPEG', 'JPG')
@@ -21,16 +22,20 @@ def get_files(path):
 	matches = []
 	for root, dirnames, filenames in os.walk(path):
 		matches = matches + [os.path.join(root, filename) for filename in filenames if filename.endswith(allowedExtensions)]
-	return matches
+	return [m.replace("\\", "/") for m in matches]
 
 files = get_files('example_images')
 
-class TimedHTTPRequestHandler(BaseHTTPRequestHandler):
+class TimedHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
-		self.send_response(200)
-		self.end_headers()
-		current_image = get_current_image()
-		self.wfile.write(current_image)
+		if self.path != "/":
+			SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+		else:
+			self.send_response(200)
+			self.end_headers()
+			current_image = get_current_image()
+			template = open("template.html").read()
+			self.wfile.write(string.Template(template).substitute(image_src = current_image, refresh_count = interval))
 
 def serve_forever():
 	PORT = 8001
